ba4b8c44368dabc9accdb3b59e8f7dfe2b1504cd
tansyDeveloper02-Arun <dasari.arun@tansycloud.com>
2020-03-20 11:43:24 +0530

    etherium connecting the Web3 added files

---
+++ reactjs_code/src/ABI_&_Contract_Address.js
@@ -0,0 +1,14 @@
+import Web3 from './web3';
+import ApartmentDetails1 from "./contracts/Apartments.json"
+
+var count = null
+if(Web3.eth.givenProvider.networkVersion === undefined){
+  count = Web3.givenProvider.networkVersion
+}else{
+  count = Web3.eth.givenProvider.networkVersion
+}
+const address = ApartmentDetails1.networks['1584579223498'].address;
+
+const abi = ApartmentDetails1.abi;
+
+export default new Web3.eth.Contract(abi,address);
